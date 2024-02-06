f9bdabe334b7c7b3848ec3ab532fe71313b0b970
G4iteir00 <149211758+G4iteir00@users.noreply.github.com>
2024-02-06 17:50:30 +0000

    add basic files

---
+++ tbn_finances-app/screens/DashboardScreen.js
@@ -0,0 +1,49 @@
+import { View, Text, StyleSheet, Button } from 'react-native';
+import { LineChart } from 'react-native-chart-kit';
+
+const DashboardScreen = ({ navigation }) => {
+    // Exemplo de dados para o gráfico de linha
+    const data = {
+        labels: ["Janeiro", "Fevereiro", "Março"],
+        datasets: [
+            {
+                data: [300, 600, 800]
+            }
+        ]
+    };
+
+    return (
+        <View style={styles.container}>
+            <Text style={styles.title}>Dashboard de Gastos x Rendas</Text>
+            <LineChart
+                data={data}
+                width={400} // largura do gráfico
+                height={220} // altura do gráfico
+                chartConfig={chartConfig}
+            />
+            {/* Adicione aqui mais componentes de UI conforme necessário */}
+        </View>
+    );
+};
+
+const chartConfig = {
+    backgroundGradientFrom: "#fff",
+    backgroundGradientTo: "#fff",
+    color: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`,
+    // outras configurações de estilo
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    title: {
+        fontSize: 20,
+        fontWeight: 'bold',
+    },
+    // outros estilos
+});
+
+export default DashboardScreen;
---
+++ tbn_finances-app/screens/ExpenseListScreen.js
@@ -0,0 +1,16 @@
+
+import { View, Text, StyleSheet, Button } from 'react-native';
+
+const ExpenseListScreen = ({ navigation }) => {
+    // Lógica para listar e adicionar gastos
+
+    return (
+        <View style={styles.container}>
+            <Text style={styles.title}>Lista de Gastos</Text>
+            {/* Adicione aqui a lista de gastos e botões */}
+        </View>
+    );
+};
+
+
+export default ExpenseListScreen;
